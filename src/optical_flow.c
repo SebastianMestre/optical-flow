44074a709df81be36ce5c817a277dfe6960236e1
SebastianMestre <mestresebastianunr@gmail.com>
2021-06-03 20:15:52 -0300

    move forward in the implementation of the algorithm

--- src/optical_flow.c
+++ src/optical_flow.c
@@ -18,29 +18,43 @@ void optical_flow(struct bmp frame, struct bmp next_frame) {
 	assert(frame.width == next_frame.width);
 	assert(frame.height == next_frame.height);
 
-	uint32_t const equation_count = (BOX_SIZE - 1) * (BOX_SIZE - 1);
+	uint32_t const width = frame.width - BOX_SIZE;
+	uint32_t const height = frame.height - BOX_SIZE;
 
-	Matrix A = matrix_make(equation_count, 2);
+	uint32_t const equation_count = (BOX_SIZE - 1) * (BOX_SIZE - 1);
 
 	for (int i = 0; i < height; ++i) {
 		for (int j = 0; j < width; ++j) {
 
+			Matrix A = matrix_make(equation_count, 2);
+			Matrix b = matrix_make(equation_count, 1);
+
 			int k = 0;
 			for (int ii = 0; ii < BOX_SIZE-1; ++ii) {
 				for(int jj = 0; jj < BOX_SIZE-1; ++jj) {
-					double const fp = intensity(at(bmp, i+ii,   j+jj));
-					double const fx = intensity(at(bmp, i+ii+1, j+jj));
-					double const fy = intensity(at(bmp, i+ii,   j+jj+1));
+					double const fp = intensity(at(frame,      i+ii,   j+jj));
+					double const fx = intensity(at(frame,      i+ii+1, j+jj));
+					double const fy = intensity(at(frame,      i+ii,   j+jj+1));
+					double const ft = intensity(at(next_frame, i+ii,   j+jj));
 
 					double const Ix = fx - fp;
 					double const Iy = fy - fp;
+					double const It = ft - fp;
 
-					matrix_set(A, k, 0, Ix);
-					matrix_set(A, k, 1, Iy);
+					matrix_set(A, k, 0,  Ix);
+					matrix_set(A, k, 1,  Iy);
+					matrix_set(b, k, 0, -It);
 
 					k += 1;
 				}
 			}
+
+			Matrix At = matrix_transpose(A);
+			Matrix AtA = matrix_product(At, A);
+
+			Matrix Atb = matrix_product(At, b);
+
+			// solve(AtA * x = Atb);
 		}
 	}
 }
