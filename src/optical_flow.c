baacdc675a3129782762b0438cd7c57dde9084d2
Sebastián Mestre <36825825+SebastianMestre@users.noreply.github.com>
2021-06-03 22:17:45 -0300

    I be flowin (#3)
    * check that current and next frame resolution match
    
    * move forward in the implementation of the algorithm
    
    * convert [0-255] to [0.0-1.0]
    
    * compute optical flow vector, and do cleanup
    
    * tighten up some indices
    
    * add missing #include and #pragma in matrix.h

--- src/optical_flow.c
+++ src/optical_flow.c
@@ -1,8 +1,10 @@
 #include "optical_flow.h"
 #include "matrix.h"
 
+#include <assert.h>
+
 static double intensity(struct rgb rgb) {
-	return 0.299 * rgb.r + 0.587 * rgb.g + 0.114 * rgb.b;
+	return (0.299 / 255) * rgb.r + (0.587 / 255) * rgb.g + (0.114 / 255) * rgb.b;
 }
 
 static struct rgb at(struct bmp bmp, int x, int y) {
@@ -12,33 +14,58 @@ static struct rgb at(struct bmp bmp, int x, int y) {
 #define BOX_RADIUS 2
 #define BOX_SIZE (2*(BOX_RADIUS)+1)
 
-void optical_flow(struct bmp bmp) {
-	uint32_t const width = bmp.width - BOX_SIZE;
-	uint32_t const height = bmp.height - BOX_SIZE;
+void optical_flow(struct bmp frame, struct bmp next_frame) {
+	assert(frame.width == next_frame.width);
+	assert(frame.height == next_frame.height);
 
-	uint32_t const equation_count = (BOX_SIZE - 1) * (BOX_SIZE - 1);
+	uint32_t const width = frame.width - BOX_SIZE;
+	uint32_t const height = frame.height - BOX_SIZE;
 
-	Matrix A = matrix_make(equation_count, 2);
+	uint32_t const equation_count = BOX_SIZE * BOX_SIZE;
 
 	for (int i = 0; i < height; ++i) {
 		for (int j = 0; j < width; ++j) {
 
+			Matrix A = matrix_make(equation_count, 2);
+			Matrix b = matrix_make(equation_count, 1);
+
 			int k = 0;
-			for (int ii = 0; ii < BOX_SIZE-1; ++ii) {
-				for(int jj = 0; jj < BOX_SIZE-1; ++jj) {
-					double const fp = intensity(at(bmp, i+ii,   j+jj));
-					double const fx = intensity(at(bmp, i+ii+1, j+jj));
-					double const fy = intensity(at(bmp, i+ii,   j+jj+1));
+			for (int ii = 0; ii < BOX_SIZE; ++ii) {
+				for(int jj = 0; jj < BOX_SIZE; ++jj) {
+					double const fp = intensity(at(frame,      i+ii,   j+jj));
+					double const fx = intensity(at(frame,      i+ii+1, j+jj));
+					double const fy = intensity(at(frame,      i+ii,   j+jj+1));
+					double const ft = intensity(at(next_frame, i+ii,   j+jj));
 
 					double const Ix = fx - fp;
 					double const Iy = fy - fp;
+					double const It = ft - fp;
 
-					matrix_set(A, k, 0, Ix);
-					matrix_set(A, k, 1, Iy);
+					matrix_set(A, k, 0,  Ix);
+					matrix_set(A, k, 1,  Iy);
+					matrix_set(b, k, 0, -It);
 
 					k += 1;
 				}
 			}
+
+			Matrix At = matrix_transpose(A);
+			Matrix AtA = matrix_product(At, A);
+
+			Matrix Atb = matrix_product(At, b);
+
+			// V -- optical flow vector (2 component)
+			Vector V = matrix_solve(AtA, as_vector_from_column(Atb));
+
+			// TODO: do something with V vector
+
+			vector_free(V);
+
+			matrix_free(Atb);
+			matrix_free(AtA);
+			matrix_free(At);
+			matrix_free(b);
+			matrix_free(A);
 		}
 	}
 }
