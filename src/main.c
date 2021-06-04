28963eccb3184feeffb4f9efbc1c68064c4bba31
SebastianMestre <mestresebastianunr@gmail.com>
2021-06-03 23:44:24 -0300

    implement a runner program

--- src/main.c
+++ src/main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "optical_flow.h"
 
 int main() {
-	printf("Hello, algebra!\n");
+	FILE* f1 = fopen("img/soft1.bmp", "r");
+	FILE* f2 = fopen("img/soft2.bmp", "r");
+
+	struct bmp in1 = bmp_read(f1);
+	struct bmp in2 = bmp_read(f2);
+
+	fclose(f1);
+	fclose(f2);
+
+	struct bmp flow = optical_flow(in1, in2);
+
+	bmp_write("result.bmp", flow);
+
+	free(in1.data);
+	free(in2.data);
+	free(flow.data);
 }
